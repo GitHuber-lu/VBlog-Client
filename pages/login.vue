838fdbb034cceb00c24acd7ac956bf722ee69ce5
zhaolu <zhaolu@xinshiyun.com>
2018-04-11 22:43:15 +0800

    add

--- pages/login.vue
+++ pages/login.vue
@@ -4,11 +4,11 @@
       <vblog-menu></vblog-menu>
     </el-header>
     <el-main>
-        <div class="login">
-            <el-input class="mb-2" v-model="username" placeholder="username"></el-input>
-            <el-input class="mb-2" v-model="password" placeholder="password" type="password"></el-input>
-            <el-button type="primary" @click="login">登录</el-button>
-        </div>
+      <div class="login">
+        <el-input class="mb-2" v-model="username" placeholder="username"></el-input>
+        <el-input class="mb-2" v-model="password" placeholder="password" type="password"></el-input>
+        <el-button type="primary" @click="login">登录</el-button>
+      </div>
     </el-main>
     <el-footer>
       <vblog-footer></vblog-footer>
@@ -17,16 +17,16 @@
 </template>
 
 <script>
-import vblogMenu from "~/components/menu.vue";
-import vblogFooter from "~/components/footer.vue";
-import md5 from "md5";
+import vblogMenu from '~/components/menu.vue'
+import vblogFooter from '~/components/footer.vue'
+import md5 from 'md5'
 
 export default {
   data() {
     return {
-      username: "",
-      password: ""
-    };
+      username: '',
+      password: ''
+    }
   },
   components: {
     vblogMenu,
@@ -34,22 +34,22 @@ export default {
   },
   methods: {
     login() {
-      this.$fetch("/login", {
+      this.$fetch('/login', {
         username: this.username,
         password: md5(this.password)
       }).then(res => {
         if (res.code == 200) {
-          this.$router.push("/manage");
-          return;
+          this.$router.push('/manage')
+          return
         }
         this.$notify.error({
-          title: "提示",
-          message: "用户名或密码错误",
+          title: '提示',
+          message: '用户名或密码错误',
           duration: 2000
-        });
-      });
+        })
+      })
     }
   }
-};
+}
 </script>
 
--- pages/manage.vue
+++ pages/manage.vue
@@ -1,3 +1,100 @@
 <template>
-    <p>这里是后台管理页面</p>
+    <el-container>
+        <el-main>
+            <el-row>
+                <el-col :span="4">
+                    <div class="el-menu-box">
+                        <el-menu default-active="2" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose" background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
+                            <el-submenu index="1">
+                                <template slot="title">
+                                    <i class="el-icon-location"></i>
+                                    <span>导航一</span>
+                                </template>
+                                <el-menu-item-group>
+                                    <template slot="title">分组一</template>
+                                    <el-menu-item index="1-1">选项1</el-menu-item>
+                                    <el-menu-item index="1-2">选项2</el-menu-item>
+                                </el-menu-item-group>
+                                <el-menu-item-group title="分组2">
+                                    <el-menu-item index="1-3">选项3</el-menu-item>
+                                </el-menu-item-group>
+                                <el-submenu index="1-4">
+                                    <template slot="title">选项4</template>
+                                    <el-menu-item index="1-4-1">选项1</el-menu-item>
+                                </el-submenu>
+                            </el-submenu>
+                            <el-menu-item index="2">
+                                <i class="el-icon-menu"></i>
+                                <span slot="title">导航二</span>
+                            </el-menu-item>
+                            <el-menu-item index="3" disabled>
+                                <i class="el-icon-document"></i>
+                                <span slot="title">导航三</span>
+                            </el-menu-item>
+                            <el-menu-item index="4">
+                                <i class="el-icon-setting"></i>
+                                <span slot="title">导航四</span>
+                            </el-menu-item>
+                        </el-menu>
+                    </div>
+                </el-col>
+                <el-col :span="20">
+                    <el-breadcrumb class="el-breadcrumb" separator="/">
+                        <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+                        <el-breadcrumb-item>
+                            <a href="/">活动管理</a>
+                        </el-breadcrumb-item>
+                        <el-breadcrumb-item>活动列表</el-breadcrumb-item>
+                        <el-breadcrumb-item>活动详情</el-breadcrumb-item>
+                    </el-breadcrumb>
+                </el-col>
+            </el-row>
+        </el-main>
+    </el-container>
 </template>
+
+<script>
+import vblogMenu from '~/components/menu.vue'
+import vblogFooter from '~/components/footer.vue'
+
+export default {
+  data() {
+    return {
+      username: '',
+      password: ''
+    }
+  },
+  components: {
+    vblogMenu,
+    vblogFooter
+  },
+  methods: {
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath)
+    },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath)
+    }
+  }
+}
+</script>
+<style scoped>
+.el-menu-box {
+  height: 100vh;
+  background-color: rgb(84, 92, 100);
+}
+.el-main {
+  padding: 0;
+  height: 100vh;
+}
+.el-breadcrumb {
+  height: 60px;
+  line-height: 60px;
+  background-color: #eee;
+  padding-left: 20px;
+}
+</style>
+
+
+
+

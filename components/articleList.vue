28362acb657cb2897d349a67c6f88ccd8c529b8c
zhaolu <zhaolu@xinshiyun.com>
2018-04-18 21:56:09 +0800

    add

--- components/articleList.vue
+++ components/articleList.vue
@@ -1,10 +1,30 @@
 <template>
   <ul class="article-list">
     <li class="item" v-for="item in articleList" :key="item._id">
-      <nuxt-link :to="{name:'article-id',params:{ id: item.id }}">
-        <div class="title">{{item.title}}</div>
-        <div class="content">{{item.content}}</div>
-      </nuxt-link>
+      <div class="top">
+        <nuxt-link class="title" :to="{name:'article-id',params:{ id: item.id }}">{{item.title}}</nuxt-link>
+        <p class="content">{{item.content}}</p>
+      </div>
+      <div class="down">
+        <ul class="info">
+          <li>
+            <i class="el-icon-edit"></i>
+            <span class="txt">Bingo</span>
+          </li>
+          <li>
+            <i class="el-icon-time"></i>
+            <span class="txt">2018.08.08</span>
+          </li>
+          <li>
+            <i class="el-icon-view"></i>
+            <span class="txt">120</span>
+          </li>
+        </ul>
+        <nuxt-link class="link" :to="{name:'article-id',params:{ id: item.id }}">
+          <i class="el-icon-menu"></i>
+          <span clas="txt">阅读</span>
+        </nuxt-link>
+      </div>
     </li>
   </ul>
 </template>
@@ -33,25 +53,55 @@ export default {
   margin-top: 0;
 }
 .article-list .item {
+  display: flex;
+  flex-direction: column;
   border-top: 1px dashed #ccc;
-  padding: 14px;
   background-color: #fff;
   list-style: none;
   min-height: 180px;
+  justify-content: space-between
 }
 .article-list .item:first-child {
   border-top: none;
 }
 .article-list .title {
-  margin-bottom: 10px;
   font-weight: bolder;
 }
 .article-list .content {
   font-size: 14px;
+  line-height: 24px;
+  margin-top: 10px;
 }
 .article-list .item > a {
   text-decoration: none;
   color: #333;
+}
+.article-list .info li {
+  display: inline-block;
+  vertical-align: middle;
+  padding: 0 20px;
+}
+.article-list .down {
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  background-color: #f5f7fa;
+  line-height: 30px;
+  font-size: 12px;
+}
+.article-list .top {
+  padding: 14px;
+}
+.article-list i {
+  margin-right: 6px;
+  vertical-align: middle;
+}
+.article-list .txt {
+  vertical-align: middle;
+  font-weight: 500;
+}
+.article-list .down .link {
+  padding-right: 20px;
 }
 </style>
 
--- pages/index.vue
+++ pages/index.vue
@@ -9,7 +9,7 @@
           <el-col :span="16">
             <div class="grid-content">
               <article-list></article-list>
-              <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page="currentPage4" :page-sizes="[100, 200, 300, 400]" :page-size="100" layout="total, sizes, prev, pager, next, jumper" :total="400">
+              <el-pagination class="mt-2" @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page="currentPage4" :page-sizes="[100, 200, 300, 400]" :page-size="100" layout="total, sizes, prev, pager, next, jumper" :total="400">
               </el-pagination>
             </div>
           </el-col>
